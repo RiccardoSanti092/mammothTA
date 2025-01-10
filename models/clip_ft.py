abdc3cba8f4c304b70d4d8896d6c0be485c17cda
RiccardoSanti092 <santi.riccardo24@gmail.com>
2025-01-10 02:23:26 +0100

    virtual batch

--- models/clip_ft.py
+++ models/clip_ft.py
@@ -335,14 +335,14 @@ class CLIP(ContinualModel):
             param = {name: param for name, param in zip(self.param_names, self.delta_w)}
             image_features = func.functional_call(self.net, param, inputs)
 
-        text_features = self.net.text_features[labels.tolist()]
+        text_features = self.net.text_features[torch.unique(labels).tolist()]
         similarity = (image_features @ text_features.T).softmax(dim=-1)
         #print(f"Labels: {labels}, after % {int(self.N_CLASSES / self.N_TASKS)}: {labels % int(self.N_CLASSES / self.N_TASKS)}")
         print('\n')
         #print(f"self.N_CLASSES: {self.N_CLASSES}, self.N_TASKS: {self.N_TASKS}")
         #print(f"Expected number of classes per task: {int(self.N_CLASSES / self.N_TASKS)}")
         #print(f"image features shape: {similarity.shape}")
-        #print(f"text features shape: {similarity.shape}")
+        print(f"text features shape: {similarity.shape}")
         print(f"Similarity shape: {similarity.shape}")
 
         loss = self.loss(similarity, (labels % int(self.N_CLASSES / self.N_TASKS))) / self.args.chunks
